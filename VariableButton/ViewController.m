6ffdbc8dbcd60a1fa95fd450a178c39220c6ca3c
tankandrun <jinshundu@gmail.com>
2015-12-08 10:57:54 +0800

    Initial Commit

---
+++ VariableButton/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  VariableButton
+//
+//  Created by 金顺度 on 15/12/8.
+//  Copyright © 2015年 金顺度. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ VariableButton/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  VariableButton
+//
+//  Created by 金顺度 on 15/12/8.
+//  Copyright © 2015年 金顺度. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
